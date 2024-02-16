84df0edd27606d7faa754430fda7d581a119cb13
Thierno Dia <thiernoabda16@gmail.com>
2024-02-16 23:26:05 +0000

    Bouton prendre une photo fonctionne Ca en a pris du temps mais yuuuuuuup J'ai pas fais les commit initiaux mais la plupart de ce que j'ai fais est basé sur la documentation officielle Mettons au fur et a mesure des commits broo

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+  }
+
+  constructor() { }
+}
